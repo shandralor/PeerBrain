dd44fe5c440a2d6f18a70876d38fc596a9ca1237
William Harrison <william@williamdavidharrison.com.au>
2023-03-29 06:27:08 +0800

    Update models.py

--- server/models.py
+++ server/models.py
@@ -2,9 +2,10 @@
 from uuid import UUID, uuid4
 from datetime import datetime
 from typing import Optional, List
-from pydantic import BaseModel, EmailStr #pylint: disable=no-name-in-module
+from pydantic import BaseModel, EmailStr  # pylint: disable=no-name-in-module
 
-class User(BaseModel): # pylint: disable=too-few-public-methods
+
+class User(BaseModel):  # pylint: disable=too-few-public-methods
     """
     Represents a user in our application.
 
@@ -16,26 +17,28 @@ class User(BaseModel): # pylint: disable=too-few-public-methods
         disabled (bool, optional): Whether the user account is disabled. Defaults to False.
         friends (List[str], optional): A list of the user's friends. Defaults to an empty list.
     """
-    
+
     username: str
     key: Optional[UUID] = uuid4()
     email: EmailStr
-    user_password : Optional[str]
+    user_password: Optional[str]
     disabled: bool = False
     friends: List[str] = []
 
-class Reader(BaseModel): # pylint: disable=too-few-public-methods
+
+class Reader(BaseModel):  # pylint: disable=too-few-public-methods
     """A helper class for the message creation process.
 
     Attributes:
         username (str or None): The username of the reader.
         encrypted_sym_key (str or None): The encrypted symmetric key for the message.
     """
-    
+
     username: str or None = None
     encrypted_sym_key: str or None = None
 
-class Thought(BaseModel): # pylint: disable=too-few-public-methods
+
+class Thought(BaseModel):  # pylint: disable=too-few-public-methods
     """
     Represents a thought with a username, key, title, content, rating, and creation date.
 
@@ -47,7 +50,7 @@ class Thought(BaseModel): # pylint: disable=too-few-public-methods
         rating (Optional[float], optional): The rating of the thought. Defaults to 0.0.
         creation_date (Optional[datetime], optional): The creation date of the thought. Defaults to None.
     """
-    
+
     username: str
     key: Optional[UUID] = uuid4()
     title: str
@@ -55,27 +58,31 @@ class Thought(BaseModel): # pylint: disable=too-few-public-methods
     rating: Optional[float] = 0.0
     creation_date: Optional[datetime]
 
-class Token(BaseModel): # pylint: disable=too-few-public-methods
+
+class Token(BaseModel):  # pylint: disable=too-few-public-methods
     """Class defining our JWT token.
 
     Attributes:
         access_token (str): The access token string.
         token_type (str): The type of token (e.g., "bearer").
     """
+
     access_token: str
     token_type: str
 
-class TokenData(BaseModel): # pylint: disable=too-few-public-methods
+
+class TokenData(BaseModel):  # pylint: disable=too-few-public-methods
     """
     Helper class for the authentication process.
 
     Attributes:
         username (str or None): The username associated with the authentication process. Defaults to None.
     """
-    
+
     username: str or None = None
 
-class UserInDB(User): # pylint: disable=too-few-public-methods
+
+class UserInDB(User):  # pylint: disable=too-few-public-methods
     """Helper class for the authentication process.
 
     This class inherits from the User class and includes an additional property for hashed password.
@@ -83,9 +90,11 @@ class UserInDB(User): # pylint: disable=too-few-public-methods
     Attributes:
         hashed_pw (str): The hashed password for the user.
     """
+
     hashed_pw: str
-    
-class PubKey(BaseModel): # pylint: disable=too-few-public-methods
+
+
+class PubKey(BaseModel):  # pylint: disable=too-few-public-methods
     """
     A class representing a public key.
 
@@ -95,9 +104,11 @@ class PubKey(BaseModel): # pylint: disable=too-few-public-methods
     Note:
         This class is a subclass of BaseModel and inherits all its attributes and methods.
     """
+
     pub_key: str
 
-class KeyStore(BaseModel): # pylint: disable=too-few-public-methods
+
+class KeyStore(BaseModel):  # pylint: disable=too-few-public-methods
     """
     A class representing a key store that stores a public key and a symmetric key.
 
@@ -105,13 +116,15 @@ class KeyStore(BaseModel): # pylint: disable=too-few-public-methods
         pub_key (str): A string representing the public key.
         symmetric_key (str): A string representing the symmetric key.
     """
-    pub_key : str
-    symmetric_key : str
-    
-class SymKeyRequest(BaseModel): # pylint: disable=too-few-public-
+
+    pub_key: str
+    symmetric_key: str
+
+
+class SymKeyRequest(BaseModel):  # pylint: disable=too-few-public-
     """
     A request model for symmetric key exchange between a user and their friend.
-    
+
     Attributes:
     -----------
     user_password : str
@@ -119,8 +132,10 @@ class SymKeyRequest(BaseModel): # pylint: disable=too-few-public-
     friend_username : str
         The username of the user's friend.
     """
-    user_password : str
-    friend_username : str
+
+    user_password: str
+    friend_username: str
+
 
 class PasswordResetUser(BaseModel):
-    username : str    
+    username: str
